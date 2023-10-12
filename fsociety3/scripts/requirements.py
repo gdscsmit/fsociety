482255725608f930ebc33fac62c76c81ae9a9eaa
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-10-12 23:27:44 +0530

    Fixed requirements

--- fsociety3/scripts/requirements.py
+++ fsociety3/scripts/requirements.py
@@ -1,3 +1,6 @@
 def install_requirements(distro):
     if distro == "arch":
-        os.system('sudo pacman -S nmap')
+        os.system('sudo pacman -S nmap yay')
+        os.system('yay -S android-sdk-platform-tools')
+    if distro == "ubuntu" or distro == "debian":
+        os.system('sudo apt-get install nmap android-platform-tools')

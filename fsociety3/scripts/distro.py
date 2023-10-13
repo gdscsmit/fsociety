99cfc87ab47eedab28051694f2079197e5c1fc9a
Indraneel Das <dasemon3002@gmail.com>
2023-10-13 16:28:55 +0530

    added error handling

--- fsociety3/scripts/distro.py
+++ fsociety3/scripts/distro.py
@@ -1,9 +1,16 @@
 def get_distro():
     id_like = None
 
-    with open('/etc/os-release', 'r') as f:
-        for line in f:
-            if line.startswith('ID_LIKE'):
-                id_like = line.split('=')[1].strip()
+    try:
+        with open('/etc/os-release', 'r') as f:
+            for line in f:
+                if line.startswith('ID_LIKE'):
+                    id_like = line.split('=')[1].strip()
+    except FileNotFoundError:
+        print("Error: File not found.")
+    except PermissionError:
+        print("Error: Permission denied.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
     return id_like

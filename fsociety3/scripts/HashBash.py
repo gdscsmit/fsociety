7aecf7055355c2a12df7170d89abf9eb30ac88a1
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-10-14 19:48:30 +0530

    Moved the function

--- fsociety3/scripts/HashBash.py
+++ fsociety3/scripts/HashBash.py
@@ -9,3 +9,7 @@ def crack_md5_hash(hashed_value, password_list):
         if hashlib.md5(password.encode()).hexdigest() == hashed_value:
             return password
     return None
+
+def open_file():
+    with open('fsociety3/assets/passwords.txt') as file:
+        potential_passwords = file.read().splitlines()

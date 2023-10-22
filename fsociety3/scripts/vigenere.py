cfc6adbea900dfd17f851113dd47d2f6d5273636
Mohammed Ali <b18@bmsit.in>
2023-10-23 02:06:37 +0530

    Added Vignere Cipher

---
+++ fsociety3/scripts/vigenere.py
@@ -0,0 +1,27 @@
+def generateKey(string, key):
+    key = list(key)
+    if len(string) == len(key):
+        return(key)
+    else:
+        for i in range(len(string) - len(key)):
+            key.append(key[i % len(key)])
+    return("" . join(key))
+     
+
+def cipherText(string, key):
+    cipher_text = []
+    for i in range(len(string)):
+        x = (ord(string[i]) + ord(key[i])) % 26
+        x += ord('A')
+        cipher_text.append(chr(x))
+
+    return("" . join(cipher_text))
+     
+
+def originalText(cipher_text, key):
+    orig_text = []
+    for i in range(len(cipher_text)):
+        x = (ord(cipher_text[i]) - ord(key[i]) + 26) % 26
+        x += ord('A')
+        orig_text.append(chr(x))
+    return("" . join(orig_text))
